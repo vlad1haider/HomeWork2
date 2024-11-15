2f3347fcc09e9474fc46357c199c358b5450d72a
Haider <59825081+vlad1haider@users.noreply.github.com>
2024-11-15 15:57:58 +0300

    Add files via upload

---
+++ RPM2/WpfApp1/App.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public int NumberOfPages { get; set; }
+    }
+
+public class MainViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Book> Books { get; set; }
+        private Book _selectedBook;
+
+        public Book SelectedBook
+        {
+            get => _selectedBook;
+            set
+            {
+                _selectedBook = value;
+                OnPropertyChanged(nameof(SelectedBook));
+            }
+        }
+
+        public ICommand AddBookCommand { get; }
+        public ICommand EditBookCommand { get; }
+        public ICommand DeleteBookCommand { get; }
+
+        public MainViewModel()
+        {
+            Books = new ObservableCollection<Book>();
+            AddBookCommand = new RelayCommand(AddBook);
+            EditBookCommand = new RelayCommand(EditBook, CanEditOrDelete);
+            DeleteBookCommand = new RelayCommand(DeleteBook, CanEditOrDelete);
+        }
+
+        private void AddBook()
+        {
+            
+        }
+
+        private void EditBook()
+        {
+            
+        }
+
+        private void DeleteBook()
+        {
+            if (SelectedBook != null)
+            {
+                Books.Remove(SelectedBook);
+            }
+        }
+
+        private bool CanEditOrDelete()
+        {
+            return SelectedBook != null;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ RPM2/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int numCounter = -1;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_B_Click(object sender, RoutedEventArgs e)
+        {
+            numCounter++;
+            LV1.Items.Add(numCounter);
+        }
+
+        private void Del_B_Click(object sender, RoutedEventArgs e)
+        {
+            if (LV1.SelectedItem != null)
+            {
+                LV1.Items.Remove(LV1.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Элемент не выбран");
+            }
+        }
+
+        private void Clear_B_Click(object sender, RoutedEventArgs e)
+        {
+            LV1.Items.Clear();
+            numCounter = -1;
+        }
+    }
+
+
+
+public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+}
